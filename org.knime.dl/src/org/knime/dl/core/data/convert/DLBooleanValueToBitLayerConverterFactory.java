ba5cb242d8e5592f755965018d3362a4050840aa
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2017-08-18 09:28:43 +0200

    AP-7701: Add new data value to layer data converters
    - boolean to bit
    - long to long
    - int to float (implication of previous commit)
    - long to double (implication of previous commit)

---
+++ org.knime.dl/src/org/knime/dl/core/data/convert/DLBooleanValueToBitLayerConverterFactory.java
@@ -0,0 +1,90 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+package org.knime.dl.core.data.convert;
+
+import org.knime.core.data.BooleanValue;
+import org.knime.core.data.ExtensibleUtilityFactory;
+import org.knime.dl.core.DLLayerData;
+import org.knime.dl.core.data.DLWritableBitBuffer;
+
+/**
+ * @author Marcel Wiedenmann, KNIME, Konstanz, Germany
+ * @author Christian Dietz, KNIME, Konstanz, Germany
+ */
+public class DLBooleanValueToBitLayerConverterFactory
+		implements DLDataValueToLayerDataConverterFactory<BooleanValue, DLWritableBitBuffer> {
+
+	@Override
+	public String getName() {
+		return ((ExtensibleUtilityFactory) BooleanValue.UTILITY).getName();
+	}
+
+	@Override
+	public Class<BooleanValue> getSourceType() {
+		return BooleanValue.class;
+	}
+
+	@Override
+	public Class<DLWritableBitBuffer> getBufferType() {
+		return DLWritableBitBuffer.class;
+	}
+
+	@Override
+	public DLDataValueToLayerDataConverter<BooleanValue, DLWritableBitBuffer> createConverter() {
+		return new DLDataValueToLayerDataConverter<BooleanValue, DLWritableBitBuffer>() {
+
+			@Override
+			public void convert(final Iterable<? extends BooleanValue> input,
+					final DLLayerData<DLWritableBitBuffer> output) {
+				final DLWritableBitBuffer buf = output.getBuffer();
+				for (final BooleanValue val : input) {
+					buf.put(val.getBooleanValue());
+				}
+			}
+		};
+	}
+}
