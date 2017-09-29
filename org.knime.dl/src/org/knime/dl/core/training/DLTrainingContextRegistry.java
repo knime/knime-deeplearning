d18d038f6c545b00e61b9090ea756f0d742f49f0
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2017-09-29 12:32:42 +0200

    AP-7527: Add extension point for training contexts

---
+++ org.knime.dl/src/org/knime/dl/core/training/DLTrainingContextRegistry.java
@@ -0,0 +1,150 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+package org.knime.dl.core.training;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.knime.dl.core.DLAbstractExtensionPointRegistry;
+import org.knime.dl.core.DLNetworkType;
+
+/**
+ * Registry for deep learning {@link DLTrainingContext training contexts}.
+ *
+ * @author Marcel Wiedenmann, KNIME, Konstanz, Germany
+ * @author Christian Dietz, KNIME, Konstanz, Germany
+ */
+public final class DLTrainingContextRegistry extends DLAbstractExtensionPointRegistry {
+
+	private static final String EXT_POINT_ID = "org.knime.dl.DLTrainingContext";
+
+	private static final String EXT_POINT_ATTR_CLASS = "DLTrainingContext";
+
+	private static DLTrainingContextRegistry instance;
+
+	/**
+	 * Returns the singleton instance.
+	 *
+	 * @return the singleton instance
+	 */
+	public static synchronized DLTrainingContextRegistry getInstance() {
+		if (instance == null) {
+			instance = new DLTrainingContextRegistry();
+		}
+		return instance;
+	}
+
+	private final HashMap<DLNetworkType<?, ?, ?>, Set<DLTrainingContext<?, ?>>> m_ctxs = new HashMap<>();
+
+	private DLTrainingContextRegistry() {
+		super(EXT_POINT_ID, EXT_POINT_ATTR_CLASS);
+		register();
+	}
+
+	// access methods:
+
+	/**
+	 * Returns all training contexts that match the given network type.
+	 *
+	 * @param networkType the network type
+	 * @return the training contexts
+	 */
+	public Collection<DLTrainingContext<?, ?>> getTrainingContextsForNetworkType(
+			final DLNetworkType<?, ?, ?> networkType) {
+		return Collections.unmodifiableCollection(m_ctxs.get(networkType));
+	}
+
+	/**
+	 * Returns the training context with the given identifier if present.
+	 *
+	 * @param identifier the identifier
+	 * @return the training context if present
+	 */
+	public Optional<DLTrainingContext<?, ?>> getTrainingContext(final String identifier) {
+		return m_ctxs.values().stream().flatMap(Set::stream).filter(ctx -> ctx.getIdentifier().equals(identifier))
+				.findFirst();
+	}
+	// :access methods
+
+	// registration:
+
+	/**
+	 * Registers the given training context.
+	 *
+	 * @param trainingContext the training context to register
+	 * @throws IllegalArgumentException if the given training context's network type is null
+	 */
+	public void registerTrainingContext(final DLTrainingContext<?, ?> trainingContext) throws IllegalArgumentException {
+		registerTrainingContextInternal(trainingContext);
+	}
+
+	@Override
+	protected void registerInternal(final IConfigurationElement elem, final Map<String, String> attrs)
+			throws Throwable {
+		registerTrainingContextInternal((DLTrainingContext<?, ?>) elem.createExecutableExtension(EXT_POINT_ATTR_CLASS));
+	}
+
+	private synchronized void registerTrainingContextInternal(final DLTrainingContext<?, ?> ctx) {
+		final DLNetworkType<?, ?, ?> networkType = ctx.getNetworkType();
+		if (networkType == null) {
+			throw new IllegalArgumentException("The training context's associated network type must not be null.");
+		}
+		Set<DLTrainingContext<?, ?>> ctxs = m_ctxs.get(networkType);
+		if (ctxs == null) {
+			ctxs = new HashSet<>();
+			m_ctxs.put(networkType, ctxs);
+		}
+		ctxs.add(ctx);
+	}
+	// :registration
+}
