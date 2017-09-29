560be091d0f860a41f4d4ebf40310fbb2b09d13e
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2017-09-29 11:45:24 +0200

    AP-7527: Add Python implementations of trainable network, adapter and config

---
+++ org.knime.dl.python/src/org/knime/dl/python/core/training/DLPythonAbstractTrainableNetwork.java
@@ -0,0 +1,120 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME GmbH, Konstanz, Germany
+ *  Website: http://www.knime.org; Email: contact@knime.org
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+package org.knime.dl.python.core.training;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+
+import org.knime.dl.core.DLInvalidContextException;
+import org.knime.dl.core.DLLayerDataSpec;
+import org.knime.dl.core.data.DLWritableBuffer;
+import org.knime.dl.core.execution.DLLayerDataBatch;
+import org.knime.dl.core.training.DLAbstractTrainableNetwork;
+import org.knime.dl.python.core.DLPythonAbstractCommands;
+import org.knime.dl.python.core.DLPythonNetwork;
+import org.knime.dl.python.core.DLPythonNetworkHandle;
+import org.knime.dl.python.core.DLPythonNetworkSpec;
+
+/**
+ * @author Marcel Wiedenmann, KNIME, Konstanz, Germany
+ * @author Christian Dietz, KNIME, Konstanz, Germany
+ */
+public abstract class DLPythonAbstractTrainableNetwork<N extends DLPythonNetwork<S>, S extends DLPythonNetworkSpec, //
+		CFG extends DLPythonTrainingConfig, C extends DLPythonAbstractCommands<?>>
+	extends DLAbstractTrainableNetwork<DLLayerDataBatch<? extends DLWritableBuffer>, //
+			DLLayerDataBatch<? extends DLWritableBuffer>, CFG, N, S, URL>
+		implements DLPythonTrainableNetwork<S> {
+
+	private C m_commands;
+
+	private DLPythonNetworkHandle m_handle;
+
+	protected DLPythonAbstractTrainableNetwork(final N network, final CFG trainingConfig) {
+		super(network, trainingConfig);
+	}
+
+	/**
+	 * The caller is responsible for {@link AutoCloseable#close() closing} the command.
+	 */
+	protected abstract C createCommands() throws DLInvalidContextException;
+
+	protected abstract void setNetworkTrainingConfig(DLPythonNetworkHandle handle, C commands, CFG config)
+			throws DLInvalidContextException, IOException;
+
+	@Override
+	public Class<?> getTrainingDataType() {
+		return DLLayerDataBatch.class;
+	}
+
+	@Override
+	public Class<?> getTargetDataType() {
+		return DLLayerDataBatch.class;
+	}
+
+	@Override
+	public void train(final Map<DLLayerDataSpec, DLLayerDataBatch<? extends DLWritableBuffer>> trainingData,
+			final Map<DLLayerDataSpec, DLLayerDataBatch<? extends DLWritableBuffer>> targetData, final long batchSize)
+			throws Exception {
+		if (m_commands == null) {
+			m_commands = createCommands();
+			m_handle = m_network.getSpec().getNetworkType().getLoader().load(m_network.getSource(),
+					m_commands.getContext());
+			setNetworkTrainingConfig(m_handle, m_commands, m_trainingConfig);
+		}
+		m_commands.setNetworkTrainingInputs(m_handle, trainingData, targetData, batchSize);
+		m_commands.trainNetwork(m_handle);
+		m_commands.getTrainingResults(m_handle);
+	}
+
+	@Override
+	public void close() throws Exception {
+		if (m_commands != null) {
+			m_commands.close();
+		}
+	}
+}
