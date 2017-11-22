ebdc30f9e4878601925bc52cf416cbeb14e9b067
David Kolb <david.kolb@knime.com>
2017-11-22 12:01:55 +0100

    Temporary workaround for multi-threading problem in keras 2.0.6
    should be further addressed later on

--- org.knime.dl.keras/py/DLKerasNetwork.py
+++ org.knime.dl.keras/py/DLKerasNetwork.py
@@ -386,7 +386,7 @@ class DLDataSupplier:
                 i = 0
             request = DLTrainingDataRequest(self._network, self._global_dict, i)
             # FIXME this shouldn't be required at all.
-            time.sleep(0.05)
+            time.sleep(0.5)
             (x, y) = self._request_func(request)
             i += 1
             yield (training_data_formatter(x, batch_size), test_data_formatter(y, batch_size))
