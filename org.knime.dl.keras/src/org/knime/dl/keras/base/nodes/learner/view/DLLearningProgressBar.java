8317387434663ee0d53f0386c6142b930e4d7120
David Kolb <david.kolb@knime.com>
2017-11-24 14:53:18 +0100

    AP-8326: Make progress bar information look nicer
    AP-8326 (DL Keras: Enhance Learner View)

--- org.knime.dl.keras/src/org/knime/dl/keras/base/nodes/learner/view/DLLearningProgressBar.java
+++ org.knime.dl.keras/src/org/knime/dl/keras/base/nodes/learner/view/DLLearningProgressBar.java
@@ -3,8 +3,10 @@ package org.knime.dl.keras.base.nodes.learner.view;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.Insets;
 
+import javax.swing.BorderFactory;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
@@ -18,8 +20,8 @@ public class DLLearningProgressBar extends JPanel{
 	private static final long serialVersionUID = 1L;
 	
 	private final JProgressBar m_progressBar;
-	private final JTextField m_progressCounter;
-	private final JTextField m_timeCounter;
+	private final JLabel m_progressCounter;
+	private final JLabel m_timeCounter;
 	
 	private static final String PROGRESS_COUNTER_FORMAT = "%d / %d";
 	
@@ -27,42 +29,38 @@ public class DLLearningProgressBar extends JPanel{
 		super(new GridBagLayout());
 		
 		m_progressBar = new JProgressBar();
-		m_progressBar.setPreferredSize(new Dimension(100, 20));
+		m_progressBar.setPreferredSize(new Dimension(100, 30));
 		
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.insets = new Insets(5, 5, 5, 5);
 		gbc.gridx = 0;
 		gbc.gridy = 0;
-        gbc.weightx = 1;
+        gbc.weightx = 0.8;
         gbc.weighty = 0;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         add(m_progressBar, gbc);
         
-        m_progressCounter = new JTextField(8);
-        m_progressCounter.setEnabled(false);
+        m_progressCounter = new JLabel();
         m_progressCounter.setText(String.format(PROGRESS_COUNTER_FORMAT, 0, 0));
+        JPanel progressCounterBox = new JPanel(new GridLayout(0, 1));
+        progressCounterBox.setBorder(BorderFactory.createTitledBorder(progressLabel + ":"));
+        progressCounterBox.add(m_progressCounter);
+        progressCounterBox.setPreferredSize(new Dimension(100, 40));
+        progressCounterBox.setMinimumSize(new Dimension(100, 40));
         gbc.insets = new Insets(5, 5, 5, 5);
-        gbc.weightx = 0;
+        gbc.weightx = 0.1;
         gbc.gridx++;
-        add(m_progressCounter, gbc);
+        add(progressCounterBox, gbc);
         
-        gbc.gridx++;
-        gbc.insets = new Insets(5, 5, 5, 5);
-        JLabel pLabel = new JLabel(progressLabel);
-        pLabel.setPreferredSize(new Dimension(100, 20));
-        add(pLabel, gbc);
-        
-        m_timeCounter = new JTextField(8);
-        m_timeCounter.setEnabled(false);
+        m_timeCounter = new JLabel();
         m_timeCounter.setText("0");
+        JPanel timeCounterBox = new JPanel(new GridLayout(0, 1));
+        timeCounterBox.setBorder(BorderFactory.createTitledBorder(timeLabel + ":"));
+        timeCounterBox.add(m_timeCounter);
+        timeCounterBox.setPreferredSize(new Dimension(100, 40));
+        timeCounterBox.setMinimumSize(new Dimension(100, 40));
         gbc.gridx++;
-        add(m_timeCounter, gbc);
-        
-        gbc.gridx++;
-        gbc.insets = new Insets(5, 5, 5, 50);
-        JLabel tLabel = new JLabel(timeLabel);
-        tLabel.setPreferredSize(new Dimension(100, 20));
-        add(tLabel, gbc);
+        add(timeCounterBox, gbc);
 	}
 	
 	public void setProgress(int progress){
