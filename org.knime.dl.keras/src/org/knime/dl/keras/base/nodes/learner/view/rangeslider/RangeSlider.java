c78776091847af2141e3591fed0b8f2a9823a5c2
David Kolb <david.kolb@knime.com>
2017-12-20 10:55:07 +0100

    AP-8502: Add range slider
    AP-8502 (DL Keras: Enhance View)

---
+++ org.knime.dl.keras/src/org/knime/dl/keras/base/nodes/learner/view/rangeslider/RangeSlider.java
@@ -0,0 +1,150 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+
+/*
+ * Original source code: Copyright © 2010 Ernest Yu. Released under MIT License.
+ * This modified version of the source code: Copyright © 2017 by Ernest Yu and KNIME AG, Switzerland
+*/
+
+package org.knime.dl.keras.base.nodes.learner.view.rangeslider;
+
+import javax.swing.JSlider;
+
+/**
+ * An extension of JSlider to select a range of values using two thumb controls.
+ * The thumb controls are used to select the lower and upper value of a range
+ * with predetermined minimum and maximum values.
+ * 
+ * <p>Note that RangeSlider makes use of the default BoundedRangeModel, which 
+ * supports an inner range defined by a value and an extent.  The upper value
+ * returned by RangeSlider is simply the lower value plus the extent.</p>
+ */
+public class RangeSlider extends JSlider {
+
+    /**
+     * Constructs a RangeSlider with default minimum and maximum values of 0
+     * and 100.
+     */
+    public RangeSlider() {
+        initSlider();
+    }
+
+    /**
+     * Constructs a RangeSlider with the specified default minimum and maximum 
+     * values.
+     */
+    public RangeSlider(int min, int max) {
+        super(min, max);
+        initSlider();
+    }
+
+    /**
+     * Initializes the slider by setting default properties.
+     */
+    private void initSlider() {
+        setOrientation(HORIZONTAL);
+    }
+
+    /**
+     * Overrides the superclass method to install the UI delegate to draw two
+     * thumbs.
+     */
+    @Override
+    public void updateUI() {
+        setUI(new RangeSliderUI(this));
+        // Update UI for slider labels.  This must be called after updating the
+        // UI of the slider.  Refer to JSlider.updateUI().
+        updateLabelUIs();
+    }
+
+    /**
+     * Returns the lower value in the range.
+     */
+    @Override
+    public int getValue() {
+        return super.getValue();
+    }
+
+    /**
+     * Sets the lower value in the range.
+     */
+    @Override
+    public void setValue(int value) {
+        int oldValue = getValue();
+        if (oldValue == value) {
+            return;
+        }
+
+        // Compute new value and extent to maintain upper value.
+        int oldExtent = getExtent();
+        int newValue = Math.min(Math.max(getMinimum(), value), oldValue + oldExtent);
+        int newExtent = oldExtent + oldValue - newValue;
+
+        // Set new value and extent, and fire a single change event.
+        getModel().setRangeProperties(newValue, newExtent, getMinimum(), 
+            getMaximum(), getValueIsAdjusting());
+    }
+
+    /**
+     * Returns the upper value in the range.
+     */
+    public int getUpperValue() {
+        return getValue() + getExtent();
+    }
+
+    /**
+     * Sets the upper value in the range.
+     */
+    public void setUpperValue(int value) {
+        // Compute new extent.
+        int lowerValue = getValue();
+        int newExtent = Math.min(Math.max(0, value - lowerValue), getMaximum() - lowerValue);
+        
+        // Set extent to set upper value.
+        setExtent(newExtent);
+    }
+}
