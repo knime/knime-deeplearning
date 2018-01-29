890d1e35ca80d26c0cef115c1f0bd94ae14f3f5f
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2018-01-29 17:15:59 +0100

    AP-8036: Remove import of werkzeug module
    This is unnecessary as it was only imported for some functionality which is already builtin.

--- org.knime.dl.python/py/DLPythonKernelService.py
+++ org.knime.dl.python/py/DLPythonKernelService.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from werkzeug.exceptions import NotImplemented
 
 # ------------------------------------------------------------------------
 #  Copyright by KNIME AG, Zurich, Switzerland
