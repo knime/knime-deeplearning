81f9268fdadbbfb3474cedda705586750f4f080a
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2018-01-29 12:09:09 +0100

    AP-8036: Introduce KNIME implementation of output consumer

---
+++ org.knime.dl/src/org/knime/dl/core/execution/DLKnimeNetworkOutputConsumer.java
@@ -0,0 +1,179 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+package org.knime.dl.core.execution;
+
+import java.nio.BufferUnderflowException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.knime.core.data.DataCell;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.streamable.RowOutput;
+import org.knime.dl.core.DLTensor;
+import org.knime.dl.core.DLTensorId;
+import org.knime.dl.core.data.DLReadableBuffer;
+import org.knime.dl.core.data.convert.DLTensorToDataCellConverter;
+import org.knime.dl.core.data.convert.DLTensorToDataCellConverterFactory;
+
+/**
+ * @author Marcel Wiedenmann, KNIME GmbH, Konstanz, Germany
+ * @author Christian Dietz, KNIME GmbH, Konstanz, Germany
+ */
+public class DLKnimeNetworkOutputConsumer implements DLNetworkOutputConsumer {
+
+	private final RowOutput m_output;
+
+	private final boolean m_append;
+
+	private final ExecutionContext m_exec;
+
+	private final Map<DLTensorId, DLKnimYXConversionHelper> m_helpers;
+
+	private DataCell[] m_temp;
+
+	public DLKnimeNetworkOutputConsumer(final RowOutput output, final boolean append,
+			final Map<DLTensorId, DLTensorToDataCellConverterFactory<?, ?>> converters, final ExecutionContext exec) {
+		m_output = output;
+		m_append = append;
+		m_exec = exec;
+		m_helpers = new HashMap<>(converters.size());
+		for (final Entry<DLTensorId, DLTensorToDataCellConverterFactory<?, ?>> entry : converters.entrySet()) {
+			final DLKnimYXConversionHelper helper = new DLKnimYXConversionHelper();
+			helper.m_factory = entry.getValue();
+			helper.m_converter = entry.getValue().createConverter();
+		}
+	}
+
+	@Override
+	public void accept(final Map<DLTensorId, DLTensor<? extends DLReadableBuffer>> tensors) {
+		if (m_temp == null) {
+			initialize(tensors);
+		}
+		for (final Entry<DLTensorId, DLTensor<? extends DLReadableBuffer>> entry : tensors.entrySet()) {
+			final DLTensorId identifier = entry.getKey();
+			final DLTensor<? extends DLReadableBuffer> tensor = entry.getValue();
+			final DLKnimYXConversionHelper helper = m_helpers.get(identifier);
+			try {
+				// converter source type and tensor element type must match
+				final DLTensorToDataCellConverter converter = helper.m_converter;
+				converter.convert(tensor, helper.m_temp, m_exec);
+			} catch (final BufferUnderflowException ex) {
+				throw new DLInvalidNetworkOutputException("Unexpected network output. Size of network output '"
+						+ tensor.getSpec().getName() + "' did not match its specification.");
+			} catch (final Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+		// must be present
+		final long batchSize = tensors.values().iterator().next().getSpec().getBatchSize().getAsLong();
+		for (int r = 0; r < batchSize; r++) {
+			int c = 0;
+			for (final DLTensorId identifier : tensors.keySet()) {
+				final DLKnimYXConversionHelper helper = m_helpers.get(identifier);
+				final DataCell[] temp = helper.m_temp;
+				for (int i = 0; i < helper.m_numOutputElements; i++, c++) {
+					m_temp[c + i] = temp[i];
+				}
+			}
+			if (m_append) {
+				// TODO: m_output.push(..) with AppendedColumnRow;
+			} else {
+				// TODO: m_output.push(..) with DefaultRow;
+			}
+		}
+	}
+
+	@Override
+	public void close() throws Exception {
+		m_output.close();
+	}
+
+	private void initialize(final Map<DLTensorId, DLTensor<? extends DLReadableBuffer>> tensors) {
+		// must be present
+		final long batchSize = tensors.values().iterator().next().getSpec().getBatchSize().getAsLong();
+		if (batchSize > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException(
+					"Batch size (" + batchSize + ") is larger than 2^31-1. This is currently not supported.");
+		}
+		long totalNumOutputElements = 0;
+		for (final Entry<DLTensorId, DLTensor<? extends DLReadableBuffer>> entry : tensors.entrySet()) {
+			final DLKnimYXConversionHelper helper = m_helpers.get(entry.getKey());
+			helper.m_numOutputElements = helper.m_factory.getDestCount(entry.getValue().getSpec());
+			if (helper.m_numOutputElements > Integer.MAX_VALUE) {
+				throw new IllegalArgumentException("Number of output elements (" + helper.m_numOutputElements
+						+ ") of output '" + entry.getValue().getSpec().getName()
+						+ "' is larger than 2^31-1. This is currently not supported.");
+			}
+			try {
+				helper.m_temp = new DataCell[Math.multiplyExact((int) batchSize, (int) helper.m_numOutputElements)];
+			} catch (final ArithmeticException e) {
+				throw new IllegalArgumentException(
+						"Number of output elements of output '" + entry.getValue().getSpec().getName()
+								+ "' times batch size is larger than 2^31-1. This is currently not supported.",
+						e);
+			}
+			totalNumOutputElements += helper.m_numOutputElements;
+		}
+		if (totalNumOutputElements > Integer.MAX_VALUE) {
+			throw new IllegalArgumentException("Number of output elements (" + totalNumOutputElements
+					+ ") is larger than 2^31-1. This is currently not supported.");
+		}
+		m_temp = new DataCell[(int) totalNumOutputElements];
+	}
+
+	private static final class DLKnimYXConversionHelper {
+
+		private DLTensorToDataCellConverterFactory<?, ?> m_factory;
+
+		private DLTensorToDataCellConverter<?, ?> m_converter;
+
+		private long m_numOutputElements;
+
+		private DataCell[] m_temp;
+	}
+}
