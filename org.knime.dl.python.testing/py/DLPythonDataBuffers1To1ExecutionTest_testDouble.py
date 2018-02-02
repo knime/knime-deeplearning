b104f38e85ab3697f6722d6c698926a07431e3ec
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2018-02-02 14:58:31 +0100

    AP-8036: Adapt buffer tests to changes introduced with part. shapes support
    Partial shape support introduced "runtime shape" cells that are attached
    to the input table sent from Java to Python. Our tests did not expect
    those "non-network input data" cells and tried to treat them as data
    buffers. This is fixed now.

--- org.knime.dl.python.testing/py/DLPythonDataBuffers1To1ExecutionTest_testDouble.py
+++ org.knime.dl.python.testing/py/DLPythonDataBuffers1To1ExecutionTest_testDouble.py
@@ -4,5 +4,6 @@ import numpy as np
 from DLPythonDataBuffers import DLPythonDoubleBuffer
 
 global test_out_data
-test_out_data = test_in_data.applymap(lambda buff : DLPythonDoubleBuffer(np.vectorize(lambda x : x * 5)(buff.array)))
-test_out_data.rename(columns={'test_in_data' : 'test_out_data'}, inplace=True)
+test_out_data = test_in_data.iloc[:,0].map(
+        lambda buff : DLPythonDoubleBuffer(np.vectorize(lambda x : x * 5)(buff.array))
+    ).to_frame('test_out_data')
