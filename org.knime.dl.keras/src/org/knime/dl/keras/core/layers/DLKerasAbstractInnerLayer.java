cb45cb8ad7692cd62ad97843378be3cb4b208ce1
Marcel Wiedenmann <marcel.wiedenmann@knime.com>
2018-04-09 22:36:09 +0200

    AP-7518: Introduce Keras layer base interface and classes
    - DLKerasLayer: Base interface. Can validate parameters, allows to fetch the spec of its output tensors and can convert itself to its Keras back end representation (i.e. a line of Python code).
    - DLKerasInnerLayer: Has parents. Can validate its input tensor specs (i.e. the output tensor specs of its parents). Also unary and binary marker interfaces.
    - DLKeraInputLayer: Has input specs (that equal their output specs).
    - Abstract base classes: Mostly convenience stuff which will come in handy when implementing all those Keras nodes.

---
+++ org.knime.dl.keras/src/org/knime/dl/keras/core/layers/DLKerasAbstractInnerLayer.java
@@ -0,0 +1,168 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+package org.knime.dl.keras.core.layers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.knime.dl.core.DLDefaultTensorId;
+import org.knime.dl.core.DLDefaultTensorSpec;
+import org.knime.dl.core.DLDimensionOrder;
+import org.knime.dl.core.DLTensorSpec;
+import org.knime.dl.util.DLUtils;
+
+/**
+ * @author Marcel Wiedenmann, KNIME GmbH, Konstanz, Germany
+ * @author Christian Dietz, KNIME GmbH, Konstanz, Germany
+ */
+public abstract class DLKerasAbstractInnerLayer extends DLKerasAbstractLayer implements DLKerasInnerLayer {
+
+    protected static void checkInputSpec(final boolean expression, final String message)
+        throws DLInvalidInputSpecException {
+        if (!expression) {
+            throw new DLInvalidInputSpecException("Invalid input specs. " + message);
+        }
+    }
+
+    private final DLKerasLayer[] m_parents;
+
+    public DLKerasAbstractInnerLayer(final String kerasIdentifier, final int numParents) {
+        super(kerasIdentifier);
+        m_parents = new DLKerasLayer[numParents];
+    }
+
+    public DLKerasAbstractInnerLayer(final String kerasIdentifier, final DLKerasLayer... parents) {
+        super(kerasIdentifier);
+        m_parents = parents;
+    }
+
+    // Convenience methods:
+
+    protected abstract void validateInputSpecs(List<Class<?>> inputElementTypes, List<Long[]> inputShapes)
+        throws DLInvalidInputSpecException;
+
+    protected abstract List<Class<?>> inferOutputElementTypes(List<Class<?>> inputElementTypes)
+        throws DLInvalidInputSpecException;
+
+    protected abstract List<Long[]> inferOutputShapes(List<Long[]> inputShape);
+
+    @Override
+    public DLKerasLayer[] getParents() {
+        return m_parents;
+    }
+
+    @Override
+    public final List<DLTensorSpec> getOutputSpecs() throws DLInvalidInputSpecException {
+        final DLInputSpecsHelperStruct inputSpecs = collectInputSpecs();
+        validateInputSpecs(inputSpecs.m_elementTypes, inputSpecs.m_shapes);
+        final List<Class<?>> outputElementTypes = inferOutputElementTypes(inputSpecs.m_elementTypes);
+        final List<Long[]> outputShapes = inferOutputShapes(inputSpecs.m_shapes);
+        final List<DLTensorSpec> outputSpecs = new ArrayList<>(outputShapes.size());
+        for (int i = 0; i < outputShapes.size(); i++) {
+            outputSpecs.add(DLDefaultTensorSpec.create(new DLDefaultTensorId("dummy"), "dummy", inputSpecs.m_batchSize,
+                outputShapes.get(i), outputElementTypes.get(i), inputSpecs.m_dimensionOrder));
+        }
+        return outputSpecs;
+    }
+
+    @Override
+    public final void validateInputSpecs() throws DLInvalidInputSpecException {
+        final DLInputSpecsHelperStruct inputSpecs = collectInputSpecs();
+        validateInputSpecs(inputSpecs.m_elementTypes, inputSpecs.m_shapes);
+    }
+
+    private DLInputSpecsHelperStruct collectInputSpecs() throws DLInvalidInputSpecException {
+        Long inputBatchSize = null;
+        final List<Long[]> inputShapes = new ArrayList<>(m_parents.length);
+        final List<Class<?>> inputElementTypes = new ArrayList<>(m_parents.length);
+        DLDimensionOrder inputDimensionOrder = null;
+        for (final DLKerasLayer parent : m_parents) {
+            final List<DLTensorSpec> parentOutputSpecs = parent.getOutputSpecs();
+            for (final DLTensorSpec parentOutputSpec : parentOutputSpecs) {
+                if (parentOutputSpec.getBatchSize().isPresent()) {
+                    final long parentBatchSize = parentOutputSpec.getBatchSize().getAsLong();
+                    if (inputBatchSize == null) {
+                        inputBatchSize = parentBatchSize;
+                    } else {
+                        checkInputSpec(inputBatchSize == parentBatchSize,
+                            "Batch sizes differ: " + inputBatchSize + " vs. " + parentBatchSize + ".");
+                    }
+                }
+                inputShapes.add(DLUtils.Shapes.shapeToLongArray(parentOutputSpec.getShape()));
+                inputElementTypes.add(parentOutputSpec.getElementType());
+                final DLDimensionOrder parentDimensionOrder = parentOutputSpec.getDimensionOrder();
+                if (inputDimensionOrder == null) {
+                    inputDimensionOrder = parentDimensionOrder;
+                } else {
+                    // TODO: implement equals/hashCode/toString in DLDefaultDimensionOrder
+                    checkInputSpec(inputDimensionOrder.equals(parentDimensionOrder),
+                        "Dimension orders differ: " + inputDimensionOrder + " vs. " + parentDimensionOrder + ".");
+                }
+            }
+        }
+        return new DLInputSpecsHelperStruct(inputBatchSize, inputShapes, inputElementTypes, inputDimensionOrder);
+    }
+
+    private static final class DLInputSpecsHelperStruct {
+
+        private final Long m_batchSize;
+
+        private final List<Long[]> m_shapes;
+
+        private final List<Class<?>> m_elementTypes;
+
+        private final DLDimensionOrder m_dimensionOrder;
+
+        private DLInputSpecsHelperStruct(final Long inputBatchSize, final List<Long[]> inputShapes,
+            final List<Class<?>> inputElementTypes, final DLDimensionOrder inputDimensionOrder) {
+            m_batchSize = inputBatchSize;
+            m_shapes = inputShapes;
+            m_elementTypes = inputElementTypes;
+            m_dimensionOrder = inputDimensionOrder;
+        }
+    }
+}
---
+++ org.knime.dl.keras/src/org/knime/dl/keras/core/layers/DLKerasAbstractUnaryInnerLayer.java
@@ -0,0 +1,97 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+package org.knime.dl.keras.core.layers;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Marcel Wiedenmann, KNIME GmbH, Konstanz, Germany
+ * @author Christian Dietz, KNIME GmbH, Konstanz, Germany
+ */
+public abstract class DLKerasAbstractUnaryInnerLayer extends DLKerasAbstractInnerLayer
+    implements DLKerasUnaryInnerLayer {
+
+    public DLKerasAbstractUnaryInnerLayer(final String kerasIdentifier) {
+        super(kerasIdentifier, 1);
+    }
+
+    public DLKerasAbstractUnaryInnerLayer(final String kerasIdentifier, final DLKerasLayer parent) {
+        super(kerasIdentifier, parent);
+    }
+
+    @Override
+    public DLKerasLayer getParent() {
+        return getParents()[0];
+    }
+
+    // Convenience methods:
+
+    protected abstract void validateInputSpec(Class<?> inputElementType, Long[] inputShape)
+        throws DLInvalidInputSpecException;
+
+    protected abstract Class<?> inferOutputElementType(Class<?> inputElementType);
+
+    protected abstract Long[] inferOutputShape(Long[] inputShape);
+
+    @Override
+    protected final void validateInputSpecs(final List<Class<?>> inputElementTypes, final List<Long[]> inputShapes)
+        throws DLInvalidInputSpecException {
+        validateInputSpec(inputElementTypes.get(0), inputShapes.get(0));
+    }
+
+    @Override
+    protected final List<Class<?>> inferOutputElementTypes(final List<Class<?>> inputElementTypes)
+        throws DLInvalidInputSpecException {
+        return Collections.singletonList(inferOutputElementType(inputElementTypes.get(0)));
+    }
+
+    @Override
+    protected final List<Long[]> inferOutputShapes(final List<Long[]> inputShape) {
+        return Collections.singletonList(inferOutputShape(inputShape.get(0)));
+    }
+}
