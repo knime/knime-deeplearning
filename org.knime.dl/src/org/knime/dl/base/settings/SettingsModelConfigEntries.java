adbb1a5863f8ad3f62e45b980274c5d4410c7a8d
Adrian Nembach <adrian.nembach@knime.com>
2018-05-03 11:14:53 +0200

    AP-8305: Static factory class for default settings model config entries
    AP-8305 (DL Framework: Harmonize DLExecutor and DLLearner)

---
+++ org.knime.dl/src/org/knime/dl/base/settings/SettingsModelConfigEntries.java
@@ -0,0 +1,107 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ */
+package org.knime.dl.base.settings;
+
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+
+/**
+ * Provides factories for {@link SettingsModelConfigEntry}s that are based on default types.
+ * 
+ * @author Adrian Nembach, KNIME GmbH, Konstanz, Germany
+ */
+public final class SettingsModelConfigEntries {
+
+    private SettingsModelConfigEntries() {
+        // static factory class for default SettingsModelConfigEntries
+    }
+
+    /**
+     * @param cfgKey the config key
+     * @param defaultValue the default string
+     * @return a {@link SettingsModelConfigEntry} that uses a {@link SettingsModelString} for serialization
+     * 
+     */
+    public static SettingsModelConfigEntry<String, SettingsModelString> createStringConfigEntry(String cfgKey, String defaultValue) {
+        return new SettingsModelConfigEntry<>(cfgKey, String.class,
+                s -> new SettingsModelString(s, defaultValue),
+                e -> new SettingsModelString(e.getEntryKey(), e.getValue()),
+                SettingsModelString::getStringValue,
+                defaultValue);
+    }
+
+    /**
+     * @param cfgKey the config key
+     * @param defaultValue the default integer value
+     * @param minValue the minimal value allowed
+     * @param maxValue the maximal value allowed
+     * @return a {@link SettingsModelConfigEntry} that uses a {@link SettingsModelIntegerBounded} fore serialization
+     * 
+     */
+    public static SettingsModelConfigEntry<Integer, SettingsModelIntegerBounded> createIntegerBoundedConfigEntry(String cfgKey,
+        int defaultValue, int minValue, int maxValue) {
+        return new SettingsModelConfigEntry<>(cfgKey, Integer.class,
+                s -> new SettingsModelIntegerBounded(cfgKey, defaultValue, minValue, maxValue),
+                e -> new SettingsModelIntegerBounded(e.getEntryKey(), e.getValue(), minValue, maxValue),
+                SettingsModelIntegerBounded::getIntValue,
+                defaultValue);
+    }
+    
+    /**
+     * @param cfgKey the config key
+     * @param defaultValue the default boolean value
+     * @return a {@link SettingsModelConfigEntry} for boolean values that uses a {@link SettingsModelBoolean} for serialization
+     */
+    public static SettingsModelConfigEntry<Boolean, SettingsModelBoolean> createBooleanConfigEntry(String cfgKey, boolean defaultValue) {
+        return new SettingsModelConfigEntry<>(cfgKey, Boolean.class,
+                s -> new SettingsModelBoolean(s, defaultValue),
+                e -> new SettingsModelBoolean(e.getEntryKey(), e.getValue()),
+                SettingsModelBoolean::getBooleanValue,
+                defaultValue);
+    }
+}
