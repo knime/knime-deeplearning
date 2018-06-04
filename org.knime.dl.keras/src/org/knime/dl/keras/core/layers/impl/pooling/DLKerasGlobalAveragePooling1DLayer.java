bb19b1edfc72a271b911d097c9caa8b70f22e833
David Kolb <david.kolb@knime.com>
2018-06-04 13:28:06 +0200

    AP-9170: Use enum
    AP-9170 (DL Keras: Create nodes for pooling layers)

--- org.knime.dl.keras/src/org/knime/dl/keras/core/layers/impl/pooling/DLKerasGlobalAveragePooling1DLayer.java
+++ org.knime.dl.keras/src/org/knime/dl/keras/core/layers/impl/pooling/DLKerasGlobalAveragePooling1DLayer.java
@@ -53,6 +53,7 @@ import org.knime.core.node.InvalidSettingsException;
 import org.knime.dl.keras.core.layers.DLConvolutionLayerUtils;
 import org.knime.dl.keras.core.layers.DLInvalidTensorSpecException;
 import org.knime.dl.keras.core.layers.DLKerasAbstractUnaryLayer;
+import org.knime.dl.keras.core.layers.DLKerasDataFormat;
 
 /**
  * @author David Kolb, KNIME GmbH, Konstanz, Germany
@@ -62,7 +63,7 @@ public final class DLKerasGlobalAveragePooling1DLayer extends DLKerasAbstractUna
     /**
      * This is hardcoded to "channels_last" in Keras
      */
-    private String m_dataFormat = "channels_last";
+    private DLKerasDataFormat m_dataFormat = DLKerasDataFormat.CHANNEL_LAST;
 
     /**
      * Constructor
@@ -84,7 +85,7 @@ public final class DLKerasGlobalAveragePooling1DLayer extends DLKerasAbstractUna
 
     @Override
     protected Long[] inferOutputShape(final Long[] inputShape) {
-        return DLConvolutionLayerUtils.computeGlobalPoolingOutputShape(inputShape, m_dataFormat);
+        return DLConvolutionLayerUtils.computeGlobalPoolingOutputShape(inputShape, m_dataFormat.value());
     }
 
     @Override
