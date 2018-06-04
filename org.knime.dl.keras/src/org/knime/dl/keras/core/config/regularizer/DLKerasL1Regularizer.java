66da656d27999974655a82112609dbe8e96ae00f
David Kolb <david.kolb@knime.com>
2018-06-04 18:35:49 +0200

    AP-9168: Add correct default value
    AP-9168 (DL Keras: Create nodes for core layers)

--- org.knime.dl.keras/src/org/knime/dl/keras/core/config/regularizer/DLKerasL1Regularizer.java
+++ org.knime.dl.keras/src/org/knime/dl/keras/core/config/regularizer/DLKerasL1Regularizer.java
@@ -60,7 +60,7 @@ import org.knime.dl.python.util.DLPythonUtils;
 public final class DLKerasL1Regularizer extends DLKerasAbstractConfigObject implements DLKerasRegularizer {
 
     @Parameter(label = "L1 regularization factor", min = "0.0000001")
-    private float m_l1;
+    private float m_l1 = 0.0000001f;
 
     /**
      */
