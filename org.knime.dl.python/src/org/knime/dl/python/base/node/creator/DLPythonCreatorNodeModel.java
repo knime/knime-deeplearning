0833ba25d5fa50ef9a9b5bb2e4499a512835c2f0
Adrian Nembach <adrian.nembach@knime.com>
2022-05-20 12:20:19 +0200

    AP-18020: Retrieve network before closing the context
    AP-18020 (Python Script node prohibits deletion of jobs folder on
    executor)

--- org.knime.dl.python/src/org/knime/dl/python/base/node/creator/DLPythonCreatorNodeModel.java
+++ org.knime.dl.python/src/org/knime/dl/python/base/node/creator/DLPythonCreatorNodeModel.java
@@ -158,9 +158,10 @@ final class DLPythonCreatorNodeModel extends DLPythonNodeModel<DLPythonCreatorNo
 						"Failed to save output deep learning network '" + outputNetworkName + "'.");
 			}
 			addNewVariables(variables);
-			shutdownContext(context);
-			return new DLNetworkPortObject[] {
+			var outputNetwork = new DLNetworkPortObject[] {
                 createOutputPortObject(loader, handle, fileStore, context, cancelable)};
+			shutdownContext(context);
+			return outputNetwork;
 		}
 	}
 
